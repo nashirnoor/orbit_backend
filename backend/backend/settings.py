f3a1a59bfc2da1b3bdcebec50f4e492df344550b
nashir <nashirnoor1718@gmail.com>
2024-09-17 15:49:47 +0530

    alkdjsf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,6 +136,8 @@ MEDIA_ROOT = 'media/'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
